86c068e7fe34737bda8cd1b77b63a8603303c673
Mateusz <mateusz@qunabu.com>
2021-04-14 17:27:20 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Editor from "./components/hh5p/editor";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Editor editorApiUrl="http://localhost:1000/api/hh5p/editor" />
     </div>
   );
 }
