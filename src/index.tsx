dd47f78b213e80e26481cd916127e44cfea7f59e
Mateusz Wojczal <mateusz@wojczal.com>
2021-09-07 15:17:44 +0200

    npm publish

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import {
+  EditorContextProvider,
+  EditorContext,
+} from "./components/hh5p/context/index";
+import Editor from "./components/hh5p/editor/index";
+import Player from "./components/hh5p/player";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+export { EditorContextProvider, EditorContext, Editor, Player };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+export default {
+  EditorContextProvider,
+  EditorContext,
+  Editor,
+  Player,
+};
