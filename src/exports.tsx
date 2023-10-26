3fbdc81233807facea28d20b35ca2872651faf64
Szymon Dolnik <szymon2407@hotmail.com>
2023-10-26 13:37:08 +0200

    fix: deleted enum

--- src/exports.tsx
+++ src/exports.tsx
@@ -8,15 +8,12 @@ import Player from "./components/hh5p/player";
 import ContextlessEditor from "./components/hh5p/contextless/editor";
 import ContextlessPlayer from "./components/hh5p/contextless/player";
 
-import { ContextlessEditorMessage } from '@escolalms/h5p-react'
-
 export {
   EditorContextProvider,
   EditorContext,
   Editor,
   Player,
   ContextlessEditor,
-  ContextlessEditorMessage,
   ContextlessPlayer,
 };
 
@@ -26,6 +23,5 @@ export default {
   Editor,
   Player,
   ContextlessEditor,
-  ContextlessEditorMessage,
   ContextlessPlayer,
 };
